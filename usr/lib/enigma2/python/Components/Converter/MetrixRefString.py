2e404c30b696ad1ee7dfcf0b428980d8cbf5ce0f
arn354 <arn354@email.de>
2013-12-22 00:26:14 +0100

    UPDATE with latest changes from https://github.com/iMaxxx/OpenStore
    Revert Font back to SetrixHD

--- usr/lib/enigma2/python/Components/Converter/MetrixRefString.py
+++ usr/lib/enigma2/python/Components/Converter/MetrixRefString.py
@@ -1,23 +1,14 @@
 #######################################################################
 #
 #
-#    ReferenceToString for Dreambox/Enigma-2
+#    MetrixRefString for Enigma-2
 #    Coded by Vali (c)2011
-#    Support: www.dreambox-tools.info
 #
 #
 #  This plugin is licensed under the Creative Commons 
 #  Attribution-NonCommercial-ShareAlike 3.0 Unported License.
 #  To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/
 #  or send a letter to Creative Commons, 559 Nathan Abbott Way, Stanford, California 94305, USA.
-#
-#  Alternatively, this plugin may be distributed and executed on hardware which
-#  is licensed by Dream Multimedia GmbH.
-#
-#
-#  This plugin is NOT free software. It is open source, you are allowed to
-#  modify it (if you keep the license), but it may not be commercially 
-#  distributed other than under the conditions noted above.
 #
 #
 #######################################################################
@@ -29,6 +20,7 @@ from Screens.InfoBar import InfoBar
 class MetrixRefString(Converter, object):
 	CURRENT = 0
 	EVENT = 1
+	
 	def __init__(self, type):
 		Converter.__init__(self, type)
 		self.CHANSEL = None
@@ -40,10 +32,25 @@ class MetrixRefString(Converter, object):
 	@cached
 	def getText(self):
 		if (self.type == self.EVENT):
-			return str(self.source.service.toString())
+			antw = str(self.source.service.toString())
+			if antw[:6] == "1:7:0:":
+				teilantw = antw.split("ORDER BY name:")
+				if len(teilantw)>1:
+					teil2antw = teilantw[1].split()
+					if len(teil2antw)>0:
+						return teil2antw[0]
+			elif antw[:6] == "1:7:1:":
+				teilantw = antw.split(".")
+				if len(teilantw)>1:
+					return teilantw[1]
+			return antw
 		elif (self.type == self.CURRENT):
 			if self.CHANSEL == None:
 				self.CHANSEL = InfoBar.instance.servicelist
+			if len(InfoBar.instance.session.dialog_stack)>1:
+				for zz in InfoBar.instance.session.dialog_stack:
+					if (str(zz[0]) == "<class 'Screens.MovieSelection.MovieSelection'>") or (str(InfoBar.instance.session.dialog_stack[1][0]) == "<class 'Screens.InfoBar.MoviePlayer'>"):
+						return self.source.text
 			vSrv = self.CHANSEL.servicelist.getCurrent()
 			return str(vSrv.toString())
 		else:
