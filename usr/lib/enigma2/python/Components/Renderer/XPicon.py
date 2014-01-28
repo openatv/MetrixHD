13f05b98bf0c66f26130db16e5f57655541528df
arn354 <arn354@email.de>
2014-01-28 10:13:00 +0100

    Update XPicon.py
    ! Fallback for nonstandard servicetypes
    ! Fallback for IPTV streams

--- usr/lib/enigma2/python/Components/Renderer/XPicon.py
+++ usr/lib/enigma2/python/Components/Renderer/XPicon.py
@@ -32,7 +32,6 @@ class XPicon(Renderer):
 		if self.instance:
 			pngname = ""
 			if what[0] != self.CHANGED_CLEAR:
-				
 				sname = self.source.text
 				pos = sname.rfind(':')
 				if pos != -1:
@@ -40,6 +39,15 @@ class XPicon(Renderer):
 				pngname = self.nameCache.get(sname, "")
 				if pngname == "":
 					pngname = self.findPicon(sname)
+					if pngname == "":
+						fields = sname.split('_', 3)
+						if len(fields) > 2 and fields[2] != '2':
+							#fallback to 1 for tv services with nonstandard servicetypes
+							fields[2] = '1'
+						if fields[0] == '4097':
+							#fallback to 1 for IPTV streams
+							fields[0] = '1'
+						pngname = self.findPicon('_'.join(fields))
 					if pngname != "":
 						self.nameCache[sname] = pngname
 			if pngname == "": # no picon for service found
