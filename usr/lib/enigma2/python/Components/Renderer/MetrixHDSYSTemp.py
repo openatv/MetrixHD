6052a438aa5fb0e80037ae5df345d0724b60f3c8
iPkay <Kay.Messerschmidt@gmx.com>
2014-05-31 15:12:45 +0200

    [FIX] Infobar SYS-Temp output

--- usr/lib/enigma2/python/Components/Renderer/MetrixHDSYSTemp.py
+++ usr/lib/enigma2/python/Components/Renderer/MetrixHDSYSTemp.py
@@ -12,18 +12,20 @@ class MetrixHDSYSTemp(Renderer, VariableText):
 
 	def changed(self, what):
 		if not self.suspended:
-			tempinfo = "--"  + str('\xc2\xb0') + "C"
+			systemp = "-- "
 			try:
 				if path.exists('/proc/stb/sensors/temp0/value'):
 					out_line = popen("cat /proc/stb/sensors/temp0/value").readline()
-					tempinfo = out_line[:2]
+					systemp = out_line.replace('\n', '')
 				elif path.exists('/proc/stb/fp/temp_sensor'):
 					out_line = popen("cat /proc/stb/fp/temp_sensor").readline()
-					tempinfo = out_line[:2]
+					systemp = out_line.replace('\n', '')
+				if not systemp == "-- " and len(systemp) > 2:
+					systemp = tempinfo[:2]
 			except:
 				pass
-			self.text = tempinfo + str('\xc2\xb0') + "C"
-
+			self.text = systemp + str('\xc2\xb0') + "C"
+			
 	def onShow(self):
 		self.suspended = False
 		self.changed(None)
