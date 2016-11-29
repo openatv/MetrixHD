1b616437002e782eea10f7511aebae4544f3f223
iPkay <iPkay@gmx.com>
2016-11-29 10:10:58 +0100

    update 'MetrixHDRunningText' renderer (now same as image 'RunningText' renderer)

--- usr/lib/enigma2/python/Components/Renderer/MetrixHDRunningText.py
+++ usr/lib/enigma2/python/Components/Renderer/MetrixHDRunningText.py
@@ -18,11 +18,23 @@
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 ################################################################################
 
-from Renderer import Renderer
-from skin import parseColor, parseFont
-from enigma import eCanvas, eLabel, eTimer, eRect, ePoint, eSize, gRGB, gFont, \
+################################################################################
+# several changes made by Dr.Best <dr.best@dreambox-tools.info> (07-18-2013)
+# - I got rid of eCanvas, instead I took a widget as a parent and scroll the label directly into the widget (this saves performance (about 30%))
+# - new property: mShown --> this fixes the bug that this renderer keeps running in background when its not shown
+# - this renderer can be used in OLED display with dmm oe2.0 images
+# - due to changing to eWidget in combination with eLabel transparent flag is possible (still cpu killer!)
+# - fixed left / right scrolling , fixed nowrap-mode
+# take a look at the discussion: http://board.dreambox-tools.info/showthread.php?6050-Erweiterung-Running-Text-render
+################################################################################
+
+from enigma import eWidget, eLabel, eTimer, ePoint, eSize, gFont, \
 	RT_HALIGN_LEFT, RT_HALIGN_CENTER, RT_HALIGN_RIGHT, RT_HALIGN_BLOCK, \
 	RT_VALIGN_TOP, RT_VALIGN_CENTER, RT_VALIGN_BOTTOM, RT_WRAP
+
+from Renderer import Renderer
+from skin import parseColor, parseFont
+
 
 # scroll type:
 NONE     = 0
@@ -45,13 +57,10 @@ class MetrixHDRunningText(Renderer):
 		Renderer.__init__(self)
 		self.type     = NONE
 		self.txfont   = gFont("Regular", 14)
-		self.fcolor   = gRGB(255,255,255)
-		self.bcolor   = gRGB(0,0,0)
-		self.scolor   = None
 		self.soffset  = (0,0)
 		self.txtflags = 0
 		self.txtext   = ""
-		self.test_label = self.mTimer = self.mStartPoint = None
+		self.scroll_label = self.mTimer = self.mStartPoint = None
 		self.X = self.Y = self.W = self.H = self.mStartDelay = 0
 		self.mAlways = 1		# always move text
 		self.mStep = 1			# moving step: 1 pixel per 1 time
@@ -61,16 +70,19 @@ class MetrixHDRunningText(Renderer):
 		self.mRepeat = 0
 		self.mPageDelay = self.mPageLength = 0
 		self.lineHeight = 0		# for text height auto correction on dmm-enigma2
+		self.mShown = 0
 
-	GUI_WIDGET = eCanvas
+
+	GUI_WIDGET = eWidget
 
 	def postWidgetCreate(self, instance):
 		for (attrib, value) in self.skinAttributes:
 			if attrib == "size":
 				x, y = value.split(',')
 				self.W, self.H = int(x), int(y)
-		self.instance.setSize( eSize(self.W,self.H) )
-		self.test_label = eLabel(instance)
+		self.instance.move(ePoint(0,0))
+		self.instance.resize( eSize(self.W,self.H) )
+		self.scroll_label = eLabel(instance)
 		self.mTimer = eTimer()
 		self.mTimer.callback.append(self.movingLoop)
 
@@ -78,7 +90,7 @@ class MetrixHDRunningText(Renderer):
 		self.mTimer.stop()
 		self.mTimer.callback.remove(self.movingLoop)
 		self.mTimer = None
-		self.test_label = None
+		self.scroll_label = None
 
 	def applySkin(self, desktop, screen):
 		def retValue(val, limit, default, Min=False):
@@ -104,14 +116,13 @@ class MetrixHDRunningText(Renderer):
 				if attrib == "font":
 					self.txfont = parseFont(value, ((1,1),(1,1)))
 				elif attrib == "foregroundColor":
-					self.fcolor = parseColor(value)
-				elif attrib == "backgroundColor":
-					self.bcolor = parseColor(value)
+					self.scroll_label.setForegroundColor(parseColor(value))
 				elif attrib in ("shadowColor","borderColor"):	# fake for openpli-enigma2
-					self.scolor = parseColor(value)
+					self.scroll_label.setShadowColor(parseColor(value))
 				elif attrib == "shadowOffset":
 					x, y = value.split(',')
 					self.soffset = (int(x),int(y))
+					self.scroll_label.setShadowOffset(ePoint(self.soffset))
 				elif attrib == "borderWidth":			# fake for openpli-enigma2
 					self.soffset = (-int(value),-int(value))
 				elif attrib == "valign" and value in ("top","center","bottom"):
@@ -125,7 +136,7 @@ class MetrixHDRunningText(Renderer):
 				elif attrib == "options":
 					options = value.split(',')
 					for o in options:
-						if o.find('=') != -1:
+						if '=' in o:
 							opt, val = (x.strip() for x in o.split('=', 1))
 						else:
 							opt, val = o.strip(), ""
@@ -160,6 +171,13 @@ class MetrixHDRunningText(Renderer):
 							self.mPageLength = retValue(val, 0, self.mPageLength)
 				else:
 					attribs.append((attrib,value))
+					if attrib == "backgroundColor":
+						self.scroll_label.setBackgroundColor(parseColor(value))
+					elif attrib == "transparent":
+						self.scroll_label.setTransparent(int(value))
+					
+						
+						
 			self.skinAttributes = attribs
 		ret = Renderer.applySkin(self, desktop, screen)
 		
@@ -167,24 +185,25 @@ class MetrixHDRunningText(Renderer):
 		if self.mLoopTimeout: self.mLoopTimeout = max(self.mStepTimeout, self.mLoopTimeout)
 		if self.mPageDelay: self.mPageDelay = max(self.mStepTimeout, self.mPageDelay)
 		
-		self.test_label.setFont(self.txfont)
+		self.scroll_label.setFont(self.txfont)
 		if not (self.txtflags & RT_WRAP):
-			self.test_label.setNoWrap(1)
-		self.test_label.setVAlign(valign)
-		self.test_label.setHAlign(self.halign)
-		self.test_label.move( ePoint(self.W,self.H) )
-		self.test_label.resize( eSize(self.W,self.H) )
+			self.scroll_label.setNoWrap(1)
+		self.scroll_label.setVAlign(valign)
+		self.scroll_label.setHAlign(self.halign)
+		self.scroll_label.move( ePoint(0,0) )
+		self.scroll_label.resize( eSize(self.W,self.H) )
 		# test for auto correction text height:
 		if self.direction in (TOP,BOTTOM):
 			from enigma import fontRenderClass
 			flh = int(fontRenderClass.getInstance().getLineHeight(self.txfont) or self.txfont.pointSize/6 + self.txfont.pointSize)
-			self.test_label.setText("WQq")
-			if flh > self.test_label.calculateSize().height():
+			self.scroll_label.setText("WQq")
+			if flh > self.scroll_label.calculateSize().height():
 				self.lineHeight = flh
-			self.test_label.setText("")
+			self.scroll_label.setText("")
 		return ret
 
 	def doSuspend(self, suspended):
+		self.mShown = 1 - suspended
 		if suspended:
 			self.changed((self.CHANGED_CLEAR,))
 		else:
@@ -198,35 +217,44 @@ class MetrixHDRunningText(Renderer):
 		if what[0] == self.CHANGED_CLEAR:
 			self.txtext = ""
 			if self.instance:
-				self.instance.clear(self.bcolor)
+				self.scroll_label.setText("")
 		else:
-			self.txtext = self.source.text or ""
-			if self.instance and not self.calcMoving():
-				self.drawText(self.X, self.Y, self.W, self.H)
+			if self.mShown:
+				self.txtext = self.source.text or ""
+				if self.instance and not self.calcMoving():
+					self.scroll_label.resize(eSize(self.W,self.H))
+					self.moveLabel(self.X, self.Y)
 
-	def drawText(self, X, Y, W, H):
-		self.instance.clear(self.bcolor)
-		
-		if not self.scolor is None:
-			self.instance.writeText( eRect(X-self.soffset[0], Y-self.soffset[1], W, H), self.scolor, self.bcolor, self.txfont, self.txtext, self.txtflags )
-			self.instance.writeText( eRect(X, Y, W, H), self.fcolor, self.scolor, self.txfont, self.txtext, self.txtflags )
-		else:
-			self.instance.writeText( eRect(X-self.soffset[0], Y-self.soffset[1], W, H), self.fcolor, self.bcolor, self.txfont, self.txtext, self.txtflags )
+	def moveLabel(self, X, Y):
+		self.scroll_label.move( ePoint( X-self.soffset[0], Y-self.soffset[1] ) )
 
 	def calcMoving(self):
+		self.X = self.Y = 0
+		if not (self.txtflags & RT_WRAP):
+			self.txtext = self.txtext.replace("\xe0\x8a"," ").replace(chr(0x8A)," ").replace("\n"," ").replace("\r"," ")
+
+		self.scroll_label.setText(self.txtext)
+	
 		if self.txtext == "" or \
 		   self.type == NONE or \
-		   self.test_label is None:
+		   self.scroll_label is None:
 			return False
+
+		if self.direction in (LEFT,RIGHT) or not (self.txtflags & RT_WRAP):
+			self.scroll_label.resize(eSize(self.txfont.pointSize * len(self.txtext),self.H)) # stupid workaround, have no better idea right now...
 		
-		self.test_label.setText(self.txtext)
-		text_size = self.test_label.calculateSize()
+		text_size = self.scroll_label.calculateSize()
 		text_width = text_size.width()
 		text_height = text_size.height()
+
+		if self.direction in (LEFT,RIGHT) or not (self.txtflags & RT_WRAP):
+			text_width +=10
+		
 		self.mStop = None
 		# text height correction if necessary:
 		if self.lineHeight and self.direction in (TOP,BOTTOM):
 			text_height = max(text_height, (text_height + self.lineHeight - 1) / self.lineHeight * self.lineHeight)
+			
 		
 #		self.type =		0 - NONE; 1 - RUNNING; 2 - SWIMMING; 3 - AUTO(???)
 #		self.direction =	0 - LEFT; 1 - RIGHT;   2 - TOP;      3 - BOTTOM
@@ -329,11 +357,17 @@ class MetrixHDRunningText(Renderer):
 
 		self.xW = max(self.W, text_width)
 		self.xH = max(self.H, text_height)
+		
+		self.scroll_label.resize(eSize(self.xW,self.xH))
+		
 		if self.mStartDelay:
 			if self.direction in (LEFT,RIGHT):
-				self.drawText(self.P, self.Y, self.xW, self.xH)
+				self.moveLabel(self.P, self.Y)
 			else: # if self.direction in (TOP,BOTTOM):
-				self.drawText(self.X, self.P, self.xW, self.xH)
+				self.moveLabel(self.X, self.P)
+				
+		
+				
 		self.mCount = self.mRepeat
 		self.mTimer.start(self.mStartDelay,True)
 		return True
@@ -341,11 +375,11 @@ class MetrixHDRunningText(Renderer):
 	def movingLoop(self):
 		if self.A <= self.P <= self.B:
 			if self.direction in (LEFT,RIGHT):
-				self.drawText(self.P, self.Y, self.xW, self.xH)
+				self.moveLabel(self.P, self.Y)
 			else: # if self.direction in (TOP,BOTTOM)
-				self.drawText(self.X, self.P, self.xW, self.xH)
+				self.moveLabel(self.X, self.P)
 			timeout = self.mStepTimeout
-			if (self.mStop != None) and (self.mStop + abs(self.mStep) > self.P >= self.mStop):
+			if (self.mStop is not None) and (self.mStop + abs(self.mStep) > self.P >= self.mStop):
 				if (self.type == RUNNING) and (self.mOneShot > 0):
 					if (self.mRepeat > 0) and (self.mCount-1 <= 0): return
 					timeout = self.mOneShot
