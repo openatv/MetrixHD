627c904d567ce9513782d8596d4b3dcb8c62bd71
iPkay <iPkay@gmx.com>
2018-01-14 12:56:55 +0100

    new converter for filter the emc title added

---
+++ usr/lib/enigma2/python/Components/Converter/MetrixHDTextfilter.py
@@ -0,0 +1,49 @@
+# -*- coding: UTF-8 -*-
+from Components.Converter.Converter import Converter
+from Components.Element import cached
+
+##########################################################################
+
+class MetrixHDTextfilter(Converter):
+
+	EMC_PATHINFO = 0
+	EMC_VERSIONSINFO = 1
+	EMC_NAME_PLUS_VERSION = 2
+
+	def __init__(self, type):
+		Converter.__init__(self, type)
+
+		if type == "emc_pathinfo":
+			self.type = self.EMC_PATHINFO
+		elif type == "emc_versionsinfo":
+			self.type = self.EMC_VERSIONSINFO
+		elif type == "emc_name_plus_version":
+			self.type = self.EMC_NAME_PLUS_VERSION
+		else:
+			self.type = self.TEXT
+
+	@cached
+	def getText(self):
+		text = self.source.getText()
+		if self.type == self.EMC_PATHINFO:
+			ts = text.split('- ')
+			if len(ts) == 2 and 'emc git' in text.lower():
+				return ts[1]
+			else:
+				return text
+		elif self.type == self.EMC_VERSIONSINFO:
+			ts = text.split(' ')
+			if len(ts) > 1 and 'emc git' in text.lower():
+				return ts[1]
+			else:
+				return text
+		elif self.type == self.EMC_NAME_PLUS_VERSION:
+			ts = text.split(' ')
+			if len(ts) > 1 and 'emc git' in text.lower():
+				return 'Enhanced Movie Center - ' + ts[1]
+			else:
+				return 'Enhanced Movie Center'
+		else:
+			return text
+
+	text = property(getText)
