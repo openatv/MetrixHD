34428308abd9366ebe49dbacc7d1900a1f6e658e
Captain <captain.onboard@web.de>
2020-08-22 12:37:28 +0200

    remove pil png8 transparency hack in new version fixed

--- usr/lib/enigma2/python/Components/Renderer/MetrixHDXPicon.py
+++ usr/lib/enigma2/python/Components/Renderer/MetrixHDXPicon.py
@@ -18,38 +18,6 @@ initOtherConfig()
 from ServiceReference import ServiceReference
 import re, unicodedata
 
-def patched_chunk_tRNS(self, pos, len):
-	i16 = PngImagePlugin.i16
-	s = ImageFile._safe_read(self.fp, len)
-	if self.im_mode == "P":
-		self.im_info["transparency"] = list(map(ord, s))
-	elif self.im_mode == "L":
-		self.im_info["transparency"] = i16(s)
-	elif self.im_mode == "RGB":
-		self.im_info["transparency"] = i16(s), i16(s[2:]), i16(s[4:])
-	return s
-PngImagePlugin.PngStream.chunk_tRNS = patched_chunk_tRNS
-
-def patched_load(self):
-	if self.im and self.palette and self.palette.dirty:
-		self.im.putpalette(*self.palette.getdata())
-		self.palette.dirty = 0
-		self.palette.rawmode = None
-		try:
-			trans = self.info["transparency"]
-		except KeyError:
-			self.palette.mode = "RGB"
-		else:
-			try:
-				for i, a in enumerate(trans):
-					self.im.putpalettealpha(i, a)
-			except TypeError:
-				self.im.putpalettealpha(trans, 0)
-			self.palette.mode = "RGBA"
-	if self.im:
-		return self.im.pixel_access(self.readonly)
-Image.Image.load = patched_load
-
 class MetrixHDXPicon(Renderer):
 	searchPaths = ('/media/mmc/%s/', '/media/usb/XPicons/%s/', '/media/usb/%s/', '/%s/', '/%sx/', '/usr/share/enigma2/XPicons/%s/', '/usr/share/enigma2/%s/', '/usr/%s/', '/media/hdd/XPicons/%s/', '/media/hdd/%s/')
 
